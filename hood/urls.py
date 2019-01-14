6493adc1d3c00bbe746bf4c5d205cd8b71328fdb
hamida-mstafa <hamidamstafa@gmail.com>
2019-01-14 11:22:22 +0300

    added the project urls

--- hood/urls.py
+++ hood/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include('neighbour.urls')),
+    url(r'',include('hoods.urls')),
     url(r'^auth/',include('registration.backends.simple.urls')),
 ]
