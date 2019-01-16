3780624d6b20bfe4ef5f033b1d38db2551f20b58
hamida-mstafa <hamidamstafa@gmail.com>
2019-01-16 12:16:05 +0300

    created a registering form

--- hoods/forms.py
+++ hoods/forms.py
@@ -2,12 +2,14 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Neighbourhoods,Profile,Message,Businesses,Comments
+
 class BusinessForm(forms.ModelForm):
     details = forms.CharField( widget=forms.Textarea )
     class Meta:
         model = Businesses
         exclude = []
         fields = ['name','dp','details']
+        
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=40)
     last_name = forms.CharField(max_length=40)
